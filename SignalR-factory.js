27b1a89997128031bf71643bb5626623782e8b78
Maher <maherAshori@users.noreply.github.com>
2017-01-14 12:31:57 +0330

    Add files via upload

---
+++ SignalR-factory.js
@@ -0,0 +1,27 @@
+﻿app.factory("signalR", function () {
+    var factory = {};
+
+    factory.setHubName = function (hubName) {
+        factory.hub = hubName;
+    }
+
+    factory.connectToHub = function () {
+        return $.connection[factory.hub];
+    }
+
+    factory.client = function () {
+        var hub = factory.connectToHub();
+        return hub.client;
+    }
+
+    factory.server = function () {
+        var hub = factory.connectToHub();
+        return hub.server;
+    }
+
+    factory.connected = function (fn) {
+        return $.connection.hub.start().done(fn);
+    }
+
+    return factory;
+});
---
+++ app.js
@@ -0,0 +1,24 @@
+﻿var app = angular.module("app", []);
+
+app.controller("ctrl", function ($scope, signalR) {
+    $scope.messages = [];
+    $scope.user = {};
+
+    signalR.setHubName("chatHub");
+
+    signalR.connectToHub();
+
+    signalR.client().broadcastMessage = function (name, message) {
+        var newChat = { name: name, message: message };
+
+        $scope.$apply(function() {
+            $scope.messages.push(newChat);
+        });
+    };
+
+    signalR.connected(function () {
+        $scope.send = function() {
+            signalR.server().send($scope.user.name, $scope.user.message);
+        }
+    });
+});
